8fd32d0ea3864d7dd25791602d9df3d76a2e5ac9
Radek <radoslaw.grzymala@hotmail.com>
2025-03-21 14:08:01 +0100

    Test: Added Sign In page test    - added base url for cy config    - added command for run cy on mac

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,20 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  const email = 'radoslaw.grzymala@hotmail.com';
+  const password = 'Radko!23';
+  const username = 'radek';
 
+  beforeEach(() => {
+    cy.visit('/user/login');
+  });
+
+  // eslint-disable-next-line max-len
+  it('should log in with valid credentials and display username in header', () => {
+    cy.get('input[type="email"]').type(email);
+    cy.get('input[type="password"]').type(password);
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.nav-item a').contains(username).should('be.visible');
   });
 });
